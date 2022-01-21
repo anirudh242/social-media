9ef6a597645c1144eb2807367f15de3c7aa5ea6d
Anirudh <anirudhbhardwaj48@gmail.com>
2022-01-21 18:34:54 +0530

    Added tailwind and it works this time!

--- client/src/routes/Index.tsx
+++ client/src/routes/Index.tsx
@@ -4,8 +4,8 @@ import '../App.css';
 
 const Index: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Index</h1>
+    <div className="text-center">
+      <h1 className="text-2xl font-bold underline">Index</h1>
       <Link to="/home">Home</Link>
       <br />
       <Link to="/login">Login</Link>
